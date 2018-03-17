d0d5039f8ce7d4b2a83d0ecc298ad93d71967569
hackerter <hackerter@gmail.com>
2018-03-17 14:51:22 -0300

    Materiales y comenzando version1

---
+++ Tarea1/dif_div.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#Para la ejecución de éste código es necesario tener instalado python v2.7 o superior
+#Se necesita ademas tener instalada la paquetería python-sympy
+#Desde linux la instalación de la paquetería simbóloca se realiza---> $sudo apt-get install python-sympy
