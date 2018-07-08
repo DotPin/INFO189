b90fe14d4ea5d8e25457799a235c474dc8444e4c
hackerter <hackerter@gmail.com>
2018-07-08 16:44:55 -0400

    Actualizado

--- Tarea_3/1.py
+++ Tarea_3/1.py
@@ -6,3 +6,30 @@
 
 
 from sympy import *
+
+
+largo = 4
+
+I = ["" for x in xrange(largo)]
+
+#arreglo y otras constantes
+D = ["" for x in xrange(largo-1)]   
+
+L = ["" for x in xrange(largo-1)]
+
+Q = 1
+
+
+
+#condiciones de borde
+I[0] = 20
+I[3] = -15
+
+#llenado vector
+in_ind=0
+for i in range(1,(largo-1)):
+    I[i] = "T"+str(in_nd)    
+    in_ind += 1
+
+
+
