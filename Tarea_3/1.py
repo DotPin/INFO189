eaed2ba814fb2ac0539b6c4fbd5cb2315e841d86
hackerter <hackerter@gmail.com>
2018-07-08 20:16:26 -0400

    Actualizado

--- Tarea_3/1.py
+++ Tarea_3/1.py
@@ -6,22 +6,22 @@ import numpy as np
 
 largo = 3   #numero de elementos
 
-V = [0 for x in xrange(largo+1)]    #vector de nodos
 
-P = [0 for x in xrange(largo+1)]    #vector de polinomios
 
-I = [[0 for x in xrange(2)] for in xrange(2)] #Arreglo elementos
+I = [[0.0 for x in xrange(2)] for x in xrange(2)] #Arreglo elementos
 
-F = [0 for x in xrange(largo*2)]      #arreglo vector F
+F = [0.0 for x in xrange(largo+1)]      #arreglo vector F
 
 #arreglo y otras constantes
-D = [0 for x in xrange(largo)]   #Arreglo de variables D
+D = [0.0 for x in xrange(largo)]   #Arreglo de variables D
 
-L = [0 for x in xrange(largo)]   #Arreglo de L
+L = [0.0 for x in xrange(largo)]   #Arreglo de L
 
-Q = 1   #valor temperatura ambiente
+Q = 1.0   #valor temperatura ambiente
 
-
+for i in range(largo):
+    D[i] = float(input("Ingrese un numero para D: "))
+    L[i] = float(input("Ingrese un numero para L: "))
 
 #condiciones de borde
 #I[0] = 20
@@ -29,37 +29,38 @@ Q = 1   #valor temperatura ambiente
 
 M=[]    #vector multidimensional
 
-
 #creación multimatrices
 for k in range(largo):
     for i in range(2):
         for j in range(2):
-            I[i][j] = D[k]/L[k] + D[k]
+            I[i][j] = D[k]/L[k]
     M.append(I)
 
-mod = 0
-cnt=0
-for j in range(largo*2):
-    F[j] = (Q*L[cnt])/2
-    if j%2==0:
-        cnt+=1
-
-golbal= armaMatriz(M)
-
-c = np.linalg.solve(global, F)    #entrega resultado de vector x
-
-print(c)
-
-acm=0
-for i in range(largo+1):
-    for j in range(largo+1):
-        acm = acm + matriz[i][j]*I[j]
 
 def armaMatriz(A):#Funcion para armar la matriz global, usando las 3 matrices
     #generadas a partir de los 3 elementos
-    MatrizComp = zeros((len(A)+1),(len(A)+1))
+    MatrizComp = [[0.0 for x in xrange(len(A)+1)] for x in xrange(len(A)+1)]
     for i in range(0,len(A)):
         for j in range(0,2):
             for k in range(0,2):
                 MatrizComp[i+j][i+k] = MatrizComp[i+j][i+k] + A[i][j][k]
     return MatrizComp
+
+F[0]=(Q*L[0])/2
+
+for j in range(largo):
+    F[j+1] = F[j] + (Q*L[j])/2
+
+F[len(F)-1] = (Q*L[j])/2
+    
+gb = []
+gb = armaMatriz(M)
+
+print(gb)
+print(F)
+
+
+c = np.linalg.solve(gb, F)    #entrega resultado de vector x
+
+print(c)
+
